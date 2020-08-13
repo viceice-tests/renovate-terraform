a2cd1f5a2be672d44094db44541158a71d160e9a
Michael Kriese <michael.kriese@visualon.de>
2020-08-13 06:23:58 +0200

    feat: add tests

---
+++ new/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "terraform-providers/docker"
+      version = "2.7.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "1.2.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.12.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
---
+++ old/versions.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_providers {
+    docker = "2.7.2"
+    helm = "1.2.4"
+    kubernetes = "1.12.0"
+  }
+}
